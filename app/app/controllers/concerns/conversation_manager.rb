ab6cf58ecfddb4c717d281018fb196b4d515ab3f
yoshinbo <yoshinbo@gmail.com>
2015-05-25 13:19:15 +0000

    誤字の修正

--- app/app/controllers/concerns/conversation_manager.rb
+++ app/app/controllers/concerns/conversation_manager.rb
@@ -2,9 +2,9 @@ module ConversationManager
   extend ActiveSupport::Concern
 
   # kind can be ":user_bang, :group_bang, :user_group"
-  def create_conversation(users, kind)
+  def create_conversation(user_ids, kind)
     conversation = Conversation.create!(kind: kind)
-    for user_id in users do
+    for user_id in user_ids do
       ConversationUser.create!(
         conversation_id: conversation.id,
         user_id: user_id
